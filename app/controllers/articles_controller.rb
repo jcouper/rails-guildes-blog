62cc9179772d8bf246f99e45ce22015b1b7e78c5
jcouper <joshcouper@gmail.com>
2020-03-11 21:35:51 -0600

    added simple navigation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,16 +6,19 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
       end
-    
+
     def new
-    
+    @article = Article.new
     end
     
     def create
-        @article = Article.new(article_params)
- 
-        @article.save
+    @article = Article.new(article_params)
+    
+    if @article.save
         redirect_to @article
+    else
+        render 'new'
+    end
     end
 
 private
